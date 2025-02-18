80d4e708dd31adbb9800b23b901972fe2f1b1b67
Phat07 <nguyenhoangphat852@gmail.com>
2025-02-18 17:31:33 +0700

    ver3

--- src/test/java/com/example/demo/AuthProjectApplicationTests.java
+++ src/test/java/com/example/demo/AuthProjectApplicationTests.java
@@ -3,10 +3,8 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("test")
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class AuthProjectApplicationTests {
 
