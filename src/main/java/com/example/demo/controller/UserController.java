489b32bcb84316ecf40b27b1fb3b3be0d24f1c97
Phat07 <nguyenhoangphat852@gmail.com>
2025-02-18 19:18:40 +0700

    ver5

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -2,23 +2,40 @@ package com.example.demo.controller;
 
 import com.example.demo.entity.User;
 import com.example.demo.repository.UserRepository;
+import com.example.demo.security.services.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
-@RequestMapping("/api/users")
+@RequestMapping("/api")
 public class UserController {
     
     @Autowired
     private UserRepository userRepository;
     
-    @GetMapping
+    @Autowired
+    UserDetailsServiceImpl userDetailsService;
+    
+    @GetMapping("/users")
     @PreAuthorize("isAuthenticated()")  // Any authenticated user with valid token can access
     public List<User> getAllUsers() {
         return userRepository.findAll();
+    }
+    
+    @GetMapping("/users/{id}")
+    public ResponseEntity<?> getUserById(@PathVariable String id) {
+        try {
+            User user = userDetailsService.getUserById(id);
+            return ResponseEntity.ok(user);
+        } catch (UsernameNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
     }
 }
